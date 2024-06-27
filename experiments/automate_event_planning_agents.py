e6f207002d09fbc9777849d77674ba681a7a8542
Brain Aboze <abozejohn@gmail.com>
2024-06-27 18:29:38 +0100

    Experimented with multi agent system for automating event planning and utilizing pydantic models to produce a JSON output

---
+++ experiments/automate_event_planning_agents.py
@@ -0,0 +1,146 @@
+# Warning control
+import warnings
+warnings.filterwarnings('ignore')
+import os
+from crewai import Agent, Task, Crew
+openai_api_key = os.getenv("OPENAI_API_KEY")
+os.environ['OPENAI_API_KEY'] = openai_api_key
+openai_model = 'gpt-4o'
+os.environ["OPENAI_MODEL_NAME"] = openai_model
+serper_api_key = os.getenv("SERPER_API_KEY")
+os.environ['SERPER_API_KEY'] = serper_api_key
+
+from crewai_tools import ScrapeWebsiteTool, SerperDevTool
+
+# Initialize the tools
+search_tool = SerperDevTool()
+scrape_tool = ScrapeWebsiteTool()
+
+# Creating Agents
+# Agent 1: Venue Coordinator
+venue_coordinator = Agent(
+    role="Venue Coordinator",
+    goal="Identify and book an appropriate venue "
+    "based on event requirements",
+    tools=[search_tool, scrape_tool],
+    verbose=True,
+    backstory=(
+        "With a keen sense of space and "
+        "understanding of event logistics, "
+        "you excel at finding and securing "
+        "the perfect venue that fits the event's theme, "
+        "size, and budget constraints."
+    )
+)
+# Agent 2: Logistics Manager
+logistics_manager = Agent(
+    role='Logistics Manager',
+    goal=(
+        "Manage all logistics for the event "
+        "including catering and equipmen"
+    ),
+    tools=[search_tool, scrape_tool],
+    verbose=True,
+    backstory=(
+        "Organized and detail-oriented, "
+        "you ensure that every logistical aspect of the event "
+        "from catering to equipment setup "
+        "is flawlessly executed to create a seamless experience."
+    )
+)
+
+# Agent 3: Marketing and Communications Agent
+marketing_communications_agent = Agent(
+    role="Marketing and Communications Agent",
+    goal="Effectively market the event and "
+         "communicate with participants",
+    tools=[search_tool, scrape_tool],
+    verbose=True,
+    backstory=(
+        "Creative and communicative, "
+        "you craft compelling messages and "
+        "engage with potential attendees "
+        "to maximize event exposure and participation."
+    )
+)
+
+# Creating venue pydantic object, so agents will populate the object with information about different venues by creating different instances of it
+from pydantic import BaseModel
+# Define a Pydantic model for venue details
+# (demonstrating Output as Pydantic)
+class VenueDetails(BaseModel):
+    name: str
+    address: str
+    capacity: int
+    booking_status: str
+
+
+# Creating tasks
+venue_task = Task(
+    description="Find a venue in {event_city} "
+                "that meets criteria for {event_topic}.",
+    expected_output="All the details of a specifically chosen"
+                    "venue you found to accommodate the event.",
+    human_input=True,
+    output_json=VenueDetails,
+    output_file="venue_details.json",
+      # Outputs the venue details as a JSON file
+    agent=venue_coordinator
+)
+
+logistics_task = Task(
+    description="Coordinate catering and "
+                 "equipment for an event "
+                 "with {expected_participants} participants "
+                 "on {tentative_date}.",
+    expected_output="Confirmation of all logistics arrangements "
+                    "including catering and equipment setup.",
+    human_input=True,
+    async_execution=True, # This task will be running in parallel with other tasks that comes after it.
+    agent=logistics_manager
+)
+
+marketing_task = Task(
+    description="Promote the {event_topic} "
+                "aiming to engage at least"
+                "{expected_participants} potential attendees.",
+    expected_output="Report on marketing activities "
+                    "and attendee engagement formatted as markdown.",
+    async_execution=True,
+    output_file="marketing_report.md",  # Outputs the report as a text file
+    agent=marketing_communications_agent
+)
+
+# Creating the crew
+event_management_crew = Crew(
+    agents=[venue_coordinator, logistics_manager, marketing_communications_agent],
+    tasks=[venue_task, logistics_task, marketing_task],
+    verbose=2,
+    memory=True
+)
+
+vent_details = {
+    'event_topic': "Tech Innovation Conference",
+    'event_description': "A gathering of tech innovators "
+                         "and industry leaders "
+                         "to explore future technologies.",
+    'event_city': "San Francisco",
+    'tentative_date': "2024-09-15",
+    'expected_participants': 500,
+    'budget': 20000,
+    'venue_type': "Conference Hall"
+}
+
+event_details = {
+    'event_topic': "Tech Innovation Conference",
+    'event_description': "A gathering of tech innovators "
+                         "and industry leaders "
+                         "to explore future technologies.",
+    'event_city': "San Francisco",
+    'tentative_date': "2024-09-15",
+    'expected_participants': 500,
+    'budget': 20000,
+    'venue_type': "Conference Hall"
+}
+
+result = event_management_crew.kickoff(inputs=event_details)
