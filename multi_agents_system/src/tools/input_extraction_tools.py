3dc105a7793a299b294b849c42ca51be0ff6f346
Brain Aboze <abozejohn@gmail.com>
2024-07-01 14:56:55 +0100

    Fixes #4 - Moved summary and semantic search index to chromadb vector store using the Ephemeral Client that does not store any data on disk which is useful for fast prototying and testing

--- multi_agents_system/src/tools/input_extraction_tools.py
+++ multi_agents_system/src/tools/input_extraction_tools.py
@@ -4,115 +4,162 @@ from llama_index.core import (
     VectorStoreIndex,
     StorageContext,
     Settings,
-    load_indices_from_storage,
     get_response_synthesizer
 )
-
+from llama_index.vector_stores.chroma import ChromaVectorStore
+import chromadb
 from llama_index.core.node_parser import SentenceSplitter
 from llama_index.llms.openai import OpenAI
 from llama_index.embeddings.openai import OpenAIEmbedding
 from crewai_tools import LlamaIndexTool
+
 import os
 import logging
 import sys
 import nest_asyncio
 
+# Apply nest_asyncio to allow nested event loops
 nest_asyncio.apply()
+
+# Configure logging
 logging.basicConfig(stream=sys.stdout, level=logging.WARNING)
 logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
 
 class InputExtractionTools:
-    def __init__(self, input_dir, persist_dir, llm_type='openai', model_name="gpt-3.5-turbo"):
+    """
+    Class for extracting input documents and creating a summarization and semantic search indices.
+    """
+    def __init__(self, input_dir, llm_type='openai', model_name="gpt-3.5-turbo"):
         self.input_dir = input_dir
-        self.persist_dir = persist_dir
         self.llm_type = llm_type
         self.model_name = model_name
-        os.makedirs(self.persist_dir, exist_ok=True)
         self.documents = self.load_documents()
         self.initialize_models()
-        self.summary_indices = self.ensure_summary_indices()
-        self.vector_store_indices = self.ensure_vector_store_indices()
+        self.chroma_client = self.initialize_vector_store_client()
+        self.input_summary_index = self.create_summary_index()
+        self.input_semantic_search_index = self.create_vector_store_index()
 
     def load_documents(self):
-        reader = SimpleDirectoryReader(input_dir=self.input_dir, required_exts=['.pdf', '.docx', '.txt', '.md', 'mp3', '.mp4'])
-        documents = reader.load_data()
-        return documents
+        """
+        Load documents from the specified directory.
+        """
+        try:
+            reader = SimpleDirectoryReader(input_dir=self.input_dir, required_exts=['.pdf', '.docx', '.txt', '.md', 'mp3', '.mp4'])
+            documents = reader.load_data()
+            return documents
+        except Exception as e:
+            logging.error(f"Error loading documents: {e}")
+            return []
 
     def initialize_models(self):
-        if self.llm_type == 'openai':
-            Settings.llm = OpenAI(model=self.model_name)
-            Settings.embed_model = OpenAIEmbedding()
-        elif self.llm_type == 'mistralai':
-            pass
-        else:
-            raise ValueError(f"Unsupported LLM type: {self.llm_type}")
-        self.splitter_strategy = SentenceSplitter()
+        """
+        Initialize LLM and embedding models based on the specified type.
+        """
+        try:
+            if self.llm_type == 'openai':
+                Settings.llm = OpenAI(model=self.model_name)
+                Settings.embed_model = OpenAIEmbedding()
+            elif self.llm_type == 'mistralai':
+                pass
+            else:
+                raise ValueError(f"Unsupported LLM type: {self.llm_type}")
+            self.splitter_strategy = SentenceSplitter()
+        except Exception as e:
+            logging.error(f"Error initializing models: {e}")
 
-    def ensure_summary_indices(self):
-        summary_index_dir = os.path.join(self.persist_dir, 'summary_index')
-        if os.path.exists(summary_index_dir):
-            return self.load_index(DocumentSummaryIndex, 'summary_index')
-        else:
-            return self.create_summary_indices()
+    def initialize_vector_store_client(self):
+        """
+        Initialize the Chroma vector store client
+        """
+        try:
+            chroma_client = chromadb.EphemeralClient()
+            return chroma_client
+        except Exception as e:
+            logging.error(f"Error iinitializing vector store client: {e}")
+            return None
 
-    def ensure_vector_store_indices(self):
-        vector_store_index_dir = os.path.join(self.persist_dir, 'vector_store_index')
-        if os.path.exists(vector_store_index_dir):
-            return self.load_index(VectorStoreIndex, 'vector_store_index')
-        else:
-            return self.create_vector_store_indices()
+    def create_chroma_db_collection(self, chroma_client, collection_name):
+        """
+        Create a Chroma DB collection for storing vectors.
+        """
+        try:
+            chroma_collection = chroma_client.create_collection(collection_name)
+            vector_store_instance = ChromaVectorStore(chroma_collection=chroma_collection)
+            return vector_store_instance
+        except Exception as e:
+            logging.error(f"Error creating Chroma DB collection: {e}")
+            return None
 
-    def create_summary_indices(self):
-        response_synthesizer = get_response_synthesizer(
-            llm=Settings.llm, response_mode="tree_summarize", use_async=True
-        )
-        summary_indices = DocumentSummaryIndex.from_documents(
-            documents=self.documents,
-            llm=Settings.llm,
-            transformations=[self.splitter_strategy],
-            response_synthesizer=response_synthesizer,
-            embed_model=Settings.embed_model,
-            show_progress=True,
-            use_async=True
-        )
-        self.persist_index(summary_indices, 'summary_index')
-        return summary_indices
+    def create_summary_index(self):
+        """
+        Create a summary index for the loaded documents.
+        """
+        try:
+            summary_vector_store_instance = self.create_chroma_db_collection(
+                self.chroma_client,
+                'msc-dissertation-001-input-summary'
+            )
+            summary_storage_context = StorageContext.from_defaults(vector_store=summary_vector_store_instance)
+            response_synthesizer = get_response_synthesizer(
+                llm=Settings.llm, response_mode="tree_summarize", use_async=True
+            )
+            summary_index = DocumentSummaryIndex.from_documents(
+                documents=self.documents,
+                llm=Settings.llm,
+                transformations=[self.splitter_strategy],
+                response_synthesizer=response_synthesizer,
+                storage_context=summary_storage_context,
+                embed_model=Settings.embed_model,
+                show_progress=True,
+                use_async=True
+            )
+            return summary_index
+        except Exception as e:
+            logging.error(f"Error creating summary index: {e}")
+            return None
 
-    def create_vector_store_indices(self):
-        vector_store_indices = VectorStoreIndex.from_documents(
-            documents=self.documents,
-            llm=Settings.llm,
-            transformations=[self.splitter_strategy],
-            embed_model=Settings.embed_model,
-            show_progress=True,
-            use_async=True
-        )
-        self.persist_index(vector_store_indices, 'vector_store_index')
-        return vector_store_indices
-
-    def persist_index(self, index, subdir):
-        persist_dir = os.path.join(self.persist_dir, subdir)
-        os.makedirs(persist_dir, exist_ok=True)
-        index.storage_context.persist(persist_dir)
-
-    def load_index(self, index_class, subdir):
-        persist_dir = os.path.join(self.persist_dir, subdir)
-        storage_context = StorageContext.from_defaults(persist_dir=persist_dir)
-        indices = load_indices_from_storage(storage_context=storage_context)
-        return indices
+    def create_vector_store_index(self):
+        """
+        Create a vector store index for semantic search.
+        """
+        try:
+            semantic_search_vector_store_instance = self.create_chroma_db_collection(
+                self.chroma_client,
+                'msc-dissertation-001-input-semantic-search'
+            )
+            semantic_search_storage_context = StorageContext.from_defaults(vector_store=semantic_search_vector_store_instance)
+            vector_store_index = VectorStoreIndex.from_documents(
+                documents=self.documents,
+                llm=Settings.llm,
+                transformations=[self.splitter_strategy],
+                embed_model=Settings.embed_model,
+                storage_context=semantic_search_storage_context,
+                show_progress=True,
+                use_async=True
+            )
+            return vector_store_index
+        except Exception as e:
+            logging.error(f"Error creating vector store index: {e}")
+            return None
 
     def create_query_engine_tools(self):
-        self.summary_query_engine = self.summary_indices[0].as_query_engine(response_mode="tree_summarize", use_async=True)
-        self.vector_store_query_engine = self.vector_store_indices[0].as_query_engine(similarity_top_k=5, llm=Settings.llm)
-        self.summary_tool = LlamaIndexTool.from_query_engine(
-            self.summary_query_engine,
-            name="Summary Index Query Tool",
-            description="Use this tool to query over the uploaded preliminary documents using the summary index."
-        )
-        self.vector_store_tool = LlamaIndexTool.from_query_engine(
-            self.vector_store_query_engine,
-            name="Vector Store Index Query Tool",
-            description="Use this tool to query over the uploaded preliminary documents using the vector store index."
-        )
-        return self.summary_tool, self.vector_store_tool
-
+        """
+        Create query engine tools for interacting with the summary and vector store indices.
+        """
+        try:
+            self.summary_query_engine = self.input_summary_index.as_query_engine(response_mode="tree_summarize", use_async=True)
+            self.vector_store_query_engine = self.input_semantic_search_index.as_query_engine(similarity_top_k=5, llm=Settings.llm)
+            self.summary_tool = LlamaIndexTool.from_query_engine(
+                self.summary_query_engine,
+                name="Summary Index Query Tool",
+                description="Use this tool to query over the uploaded preliminary documents using the summary index."
+            )
+            self.vector_store_tool = LlamaIndexTool.from_query_engine(
+                self.vector_store_query_engine,
+                name="Vector Store Index Query Tool",
+                description="Use this tool to query over the uploaded preliminary documents using the vector store index."
+            )
+            return self.summary_tool, self.vector_store_tool
+        except Exception as e:
+            logging.error(f"Error creating query engine tools: {e}")
+            return None, None
