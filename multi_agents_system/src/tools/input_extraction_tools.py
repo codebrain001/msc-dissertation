45c9b37083f9e0df1f4079eebb6fe0910c7bf38a
Brain Aboze <abozejohn@gmail.com>
2024-07-02 18:52:22 +0100

    Fixed issue #5, #6 and #7

--- multi_agents_system/src/tools/input_extraction_tools.py
+++ multi_agents_system/src/tools/input_extraction_tools.py
@@ -4,8 +4,10 @@ from llama_index.core import (
     VectorStoreIndex,
     StorageContext,
     Settings,
-    get_response_synthesizer
+    get_response_synthesizer,
+    load_indices_from_storage
 )
+
 from llama_index.vector_stores.chroma import ChromaVectorStore
 import chromadb
 from llama_index.core.node_parser import SentenceSplitter
@@ -68,67 +70,83 @@ class InputExtractionTools:
             logging.error(f"Error initializing models: {e}")
 
     def initialize_vector_store_client(self):
-        """
-        Initialize the Chroma vector store client
-        """
-        try:
-            chroma_client = chromadb.EphemeralClient()
-            return chroma_client
-        except Exception as e:
-            logging.error(f"Error iinitializing vector store client: {e}")
-            return None
+            """
+            Initialize the Chroma vector store client with Persistent client that store data locally in a specified directory
+            """
+            try:
+                persist_vector_store_path = 'multi_agents_system/src/tools/chromadb/'
+                chroma_client = chromadb.PersistentClient(path=persist_vector_store_path)
+                return chroma_client
+            except Exception as e:
+                logging.error(f"Error initializing Chroma vector store client: {e}")
+                return None
 
-    def create_chroma_db_collection(self, chroma_client, collection_name):
+    def create_chroma_db_collection(self, chroma_client, collection_name, load_collection_status=False):
         """
-        Create a Chroma DB collection for storing vectors.
+        Create or load a Chroma DB collection for storing vectors.
         """
         try:
-            chroma_collection = chroma_client.create_collection(name=collection_name)
+            if load_collection_status:
+                chroma_collection = chroma_client.get_collection(name=collection_name)
+                logging.info(f"Loaded existing Chroma DB collection: {collection_name}")
+            else:
+                chroma_collection = chroma_client.create_collection(name=collection_name)
+                logging.info(f"Created new Chroma DB collection: {collection_name}")
             vector_store_instance = ChromaVectorStore(chroma_collection=chroma_collection)
             return vector_store_instance
         except Exception as e:
-                logging.error(f"Error creating Chroma DB collection: {e}")
-                return None
+            logging.error(f"Error creating or loading Chroma DB collection: {e}")
+            return None
 
-    def create_summary_index(self, collection_name='msc-dissertation-001-input-summary'):
+    def create_summary_index(self, collection_name, load_collection_status):
         """
-        Create a summary index for the loaded documents.
+        Create or load a summary index for the loaded documents.
         """
         try:
             summary_vector_store_instance = self.create_chroma_db_collection(
-                self.chroma_client,
-                collection_name
+            self.chroma_client,
+            collection_name,
+            load_collection_status
             )
             summary_storage_context = StorageContext.from_defaults(vector_store=summary_vector_store_instance)
-            response_synthesizer = get_response_synthesizer(
+            if load_collection_status:
+                summary_index = load_indices_from_storage(storage_context=summary_storage_context)
+                return summary_index
+            else:
+                response_synthesizer = get_response_synthesizer(
                 llm=Settings.llm, response_mode="tree_summarize", use_async=True
-            )
-            summary_index = DocumentSummaryIndex.from_documents(
-                documents=self.documents,
-                llm=Settings.llm,
-                transformations=[self.splitter_strategy],
-                response_synthesizer=response_synthesizer,
-                storage_context=summary_storage_context,
-                embed_model=Settings.embed_model,
-                show_progress=True,
-                use_async=True
-            )
-            return summary_index
+                )
+                summary_index = DocumentSummaryIndex.from_documents(
+                    documents=self.documents,
+                    llm=Settings.llm,
+                    transformations=[self.splitter_strategy],
+                    response_synthesizer=response_synthesizer,
+                    storage_context=summary_storage_context,
+                    embed_model=Settings.embed_model,
+                    show_progress=True,
+                    use_async=True
+                )
+                return summary_index
         except Exception as e:
-            logging.error(f"Error creating summary index: {e}")
+            logging.error(f"Error creating or loading summary index: {e}")
             return None
 
-    def create_vector_store_index(self, collection_nmae='msc-dissertation-001-input-semantic-search'):
+    def create_vector_store_index(self, collection_name, load_collection_status):
         """
         Create a vector store index for semantic search.
         """
         try:
             semantic_search_vector_store_instance = self.create_chroma_db_collection(
-                self.chroma_client,
-                collection_nmae
+            self.chroma_client,
+            collection_name,
+            load_collection_status
             )
             semantic_search_storage_context = StorageContext.from_defaults(vector_store=semantic_search_vector_store_instance)
-            vector_store_index = VectorStoreIndex.from_documents(
+            if load_collection_status:
+                semantic_search_index = load_indices_from_storage(storage_context=semantic_search_storage_context)
+                return semantic_search_index
+            else:
+                vector_store_index = VectorStoreIndex.from_documents(
                 documents=self.documents,
                 llm=Settings.llm,
                 transformations=[self.splitter_strategy],
@@ -139,7 +157,7 @@ class InputExtractionTools:
             )
             return vector_store_index
         except Exception as e:
-            logging.error(f"Error creating vector store index: {e}")
+            logging.error(f"Error creating or loading vector store index: {e}")
             return None
 
     def create_query_engine_tools(self):
@@ -161,5 +179,5 @@ class InputExtractionTools:
             )
             return self.summary_tool, self.vector_store_tool
         except Exception as e:
-            logging.error(f"Error creating query engine tools: {e}")
+            logging.error(f"Error creating or loading query engine tools: {e}")
             return None, None
