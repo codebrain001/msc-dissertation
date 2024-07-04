66c049d42ca2ab21a4c12e0eca0625ca2b36aa31
Brain Aboze <abozejohn@gmail.com>
2024-07-04 08:21:19 +0100

    Set load_collection_status to True, since GDPR document is not changing and we already have the embedding saved in the vector store, we will load it instead on creating it on  every re-run of the script

--- multi_agents_system/src/tools/compliance_tools.py
+++ multi_agents_system/src/tools/compliance_tools.py
@@ -20,6 +20,6 @@ class ComplianceTools(InputExtractionTools):
         self.documents = self.load_documents()
         self.initialize_models()
         self.chroma_client = self.initialize_vector_store_client()
-        self.gdpr_summary_index = self.create_summary_index(collection_name='gdpr-summary', load_collection_status=False)
-        self.gdpr_semantic_search_index = self.create_vector_store_index(collection_name='gdpr-semantic-search', load_collection_status=False)
+        self.gdpr_summary_index = self.create_summary_index(collection_name='gdpr-summary', load_collection_status=True)
+        self.gdpr_semantic_search_index = self.create_vector_store_index(collection_name='gdpr-semantic-search', load_collection_status=True)
         self.gdpr_summary_tool, self.gdpr_semantic_search_tool = self.create_query_engine_tools()
